3e1ed332b46b47baece962ca9084d60baea8df8f
Haris Hamid <harishamid591@gmail.com>
2025-08-13 09:03:08 +0530

    project started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold text-blue-500 underline">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
